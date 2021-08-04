f0369937b181dfb88b23f8d70004e982796c45a6
Jose Orozco <jose816orozco@gmail.com>
2021-08-04 14:51:41 -0500

    fixing tabs

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,24 +1,62 @@
 import React from 'react';
 
-export default function Blog() {
+const canines = [
+  {
+    name: 'Spot',
+    description: 'The best boy',
+    id: 1,
+  },
+  {
+    name: 'Zero',
+    description: 'A kind soul',
+    id: 2,
+  },
+  {
+    name: 'Walter',
+    description: 'Friend for life',
+    id: 3,
+  },
+];
+
+export default function Display() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      {canines.map((dog) => (
+        <portfolio name={dog.name} description={dog.description} key={dog.id} />
+      ))}
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// export default function Blog() {
+//   return (
+//     <div>
+//       <h1>Blog Page</h1>
+//       <p>
+//         Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
+//         Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
+//         dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
+//         sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
+//         sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
+//         vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
+//         libero. Class aptent taciti sociosqu ad litora torquent per conubia
+//         nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
+//         lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+//         Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
+//         in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
+//         bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
+//         est ligula suscipit velit, sed bibendum turpis dui in sapien.
+//       </p>
+//     </div>
+//   );
+// }
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+      My name is Jose, and I decided to change my occupation after 
+      five years of working in warehouses. I still like communicating with people and work in a team.
+       I have become interested in coding and these type of fields recently.
+      </p>
+    </div>
+  );
+}
